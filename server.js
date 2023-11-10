84c219d28b8d3149d663cde2973b98856e9a5bf4
manolioluigi <luigi.manolio1999@gmail.com>
2023-11-10 16:00:00 +0100

    fine milestone

--- server.js
+++ server.js
@@ -8,34 +8,37 @@ let posts = [
     {
         title: "Viaggio in montagna",
         content: "Questa è la storia del mio emozionante viaggio in montagna. Sono rimasto senza parole di fronte alla bellezza dei panorami.",
-        image: "/public/montagna.jpg",
+        image: "/montagna.jpg",
         tags: ["viaggio", "montagna", "avventura"]
     },
     {
         title: "Ricetta della settimana",
         content: "Questa settimana condivido una deliziosa ricetta: lasagne fatte in casa con amore. Spero che vi piaccia!",
-        image: "/public/lasagne.jpg",
+        image: "/lasagne.jpg",
         tags: ["cucina", "ricette", "lasagne"]
     },
     {
         title: "Esperienza di volontariato",
         content: "Ho trascorso il weekend facendo volontariato presso un rifugio per animali. È stato un'esperienza incredibilmente gratificante.",
-        image: "/public/volontariato.jpg",
+        image: "/volontariato.jpg",
         tags: ["volontariato", "animali", "solidarietà"]
     },
     {
         title: "Consigli per la lettura",
         content: "Ecco alcune delle mie raccomandazioni di lettura preferite. Spero che troviate ispirazione in questi libri straordinari.",
-        image: "/public/libri.jpg",
+        image: "/libri.jpg",
         tags: ["libri", "lettura", "consigli"]
     },
     {
         title: "Nuova avventura culinaria",
         content: "Ho sperimentato con una cucina etnica e voglio condividere la mia esperienza. Scoprirete nuovi sapori e aromi!",
-        image: "/public/cucina.jpg",
+        image: "/cucina.jpg",
         tags: ["cucina", "avventura", "sperimentazione"]
     }
 ];
+
+//file statici
+server.use(express.static("public"));
 
 //rotte
 server.get('/', (req, res) => {
