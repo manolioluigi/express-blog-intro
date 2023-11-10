f7e3ab4c786b2ba6015d6a2cc5663f0ab36e1131
manolioluigi <luigi.manolio1999@gmail.com>
2023-11-10 16:51:20 +0100

    bonus 1 e 2

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,8 +1,17 @@
-const index = (posts, req, res) => {
+const posts = require('../db/postsDb.js'); // Assicurati di specificare il percorso corretto
+
+const index = (req, res) => {
     res.format({
         html: () => {
-            const postListHtml = posts.map(post => `<li>${post.title}</li>`).join('');
-            res.send(`<ul>${postListHtml}</ul>`);
+            const postListHtml = posts.map(post => `
+                <li>
+                    <h2>${post.title}</h2>
+                    <img src="${post.image}" style="max-width: 500px;" alt="${post.title}">
+                    <p>${post.content}</p>
+                    <ul>${post.tags.map(tag => `<li>${tag}</li>`).join('')}</ul>
+                </li>
+            `).join('');
+            res.send(`<a href="http://localhost:3300/">Torna alla home</a><br><ul>${postListHtml}</ul>`);
         },
         json: () => {
             res.json(posts);
--- server.js
+++ server.js
@@ -3,50 +3,17 @@ const express = require("express");
 const server = express();
 const postsController = require("./controllers/posts.js");
 
-//array
-let posts = [
-    {
-        title: "Viaggio in montagna",
-        content: "Questa è la storia del mio emozionante viaggio in montagna. Sono rimasto senza parole di fronte alla bellezza dei panorami.",
-        image: "/montagna.jpg",
-        tags: ["viaggio", "montagna", "avventura"]
-    },
-    {
-        title: "Ricetta della settimana",
-        content: "Questa settimana condivido una deliziosa ricetta: lasagne fatte in casa con amore. Spero che vi piaccia!",
-        image: "/lasagne.jpg",
-        tags: ["cucina", "ricette", "lasagne"]
-    },
-    {
-        title: "Esperienza di volontariato",
-        content: "Ho trascorso il weekend facendo volontariato presso un rifugio per animali. È stato un'esperienza incredibilmente gratificante.",
-        image: "/volontariato.jpg",
-        tags: ["volontariato", "animali", "solidarietà"]
-    },
-    {
-        title: "Consigli per la lettura",
-        content: "Ecco alcune delle mie raccomandazioni di lettura preferite. Spero che troviate ispirazione in questi libri straordinari.",
-        image: "/libri.jpg",
-        tags: ["libri", "lettura", "consigli"]
-    },
-    {
-        title: "Nuova avventura culinaria",
-        content: "Ho sperimentato con una cucina etnica e voglio condividere la mia esperienza. Scoprirete nuovi sapori e aromi!",
-        image: "/cucina.jpg",
-        tags: ["cucina", "avventura", "sperimentazione"]
-    }
-];
-
 //file statici
 server.use(express.static("public"));
 
 //rotte
 server.get('/', (req, res) => {
-    res.send('<h1>Benvenuto nel mio blog!</h1>')
-})
+    res.send('<h1>Benvenuto nel mio blog!</h1><br><a href="http://localhost:3300/posts">Vai ai post</a>');
+});
+
 
 server.get('/posts', (req, res) => {
-    postsController.index(posts, req, res);
+    postsController.index(req, res);
 });
 
 
